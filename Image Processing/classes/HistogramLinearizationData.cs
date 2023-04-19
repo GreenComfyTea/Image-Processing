8e894d89714913c40b4baeddce93ba07a93c380f
GreenComfyTea <klunics97@gmail.com>
2023-04-19 11:28:25 +0300

    Add HistogramLinearizationData

---
+++ Image Processing/classes/HistogramLinearizationData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessing
+{
+	class HistogramLinearizationData
+	{
+		public int Xi { get; set; }
+		public double Hx { get; set; }
+		public double YHx { get; set; }
+		public double YHz { get; set; }
+		public int Zj { get; set; }
+
+		public HistogramLinearizationData(int Xi, double Hx, double YHx, double YHz, int Zj)
+		{
+			this.Xi = Xi;
+			this.Hx = Hx;
+			this.YHx = YHx;
+			this.YHz = YHz;
+			this.Zj = Zj;
+		}
+	}
+}
